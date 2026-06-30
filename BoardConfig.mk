93f25e6b6c87671e49525e237069352ae287ed06
Ayan <sairb1@users.noreply.github.com>
2026-06-30 22:09:52 +0530

    Undo 120Hz flag and fsck check removals

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,7 +166,6 @@ TW_INCLUDE_REPACKTOOLS := true
 
 # TWRP / OrangeFox compatible flags
 # Display & Touch
-TW_FRAMES_PER_SECOND := 120
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 1000
 TW_DEFAULT_BRIGHTNESS := 1000
